a5761a646103bdb513c037e5f85c46e75f89d533
adyantwarang <waranganmol@gmail.com>
2026-03-23 20:51:07 +0530

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,12 @@
+#printing a greeting message
+print("Hello world")
+
+#printing my name
+print("My name is Nairit")
+
+#printing my hoobies
+#my favourite sport specifically
+print("I love playing cricket")
+
+#printing a math equation
+print("3 + 5 is",3 + 5)
