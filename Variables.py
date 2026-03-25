a52ce48b32c9ee5282d808c66681d902db2232e9
adyantwarang <waranganmol@gmail.com>
2026-03-25 20:48:11 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,12 @@
+#creating different variables
+x = 7
+y = "john"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#user input
+name = input("Enter your name")
+print("\nhello", name, "\nWelcome to Python" )
