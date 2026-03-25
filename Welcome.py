32477fcf733df75d43fd51d1a28643a7e147681c
adyantwarang <waranganmol@gmail.com>
2026-03-25 20:38:38 +0530

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,15 @@
+#printing a sentence
+print("Welcome to the world of programming")
+
+#printing a number
+print(4)
+
+#next line after pint
+print("Hello Python \n")
+
+#passing multiple arguments
+print("Hello", 5)
+
+#end of argument
+print("Welcome to", end="*")
+
