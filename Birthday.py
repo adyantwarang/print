0aeeb3c3526944e31be1fca9c541ebb51e1bf0e8
adyantwarang <waranganmol@gmail.com>
2026-03-27 14:58:37 +0530

    Add files via upload

---
+++ Birthday.py
@@ -0,0 +1,11 @@
+#storing 5 birthdays
+a = 2.8.1980
+b = 10.9.1982
+c = 12.6.2017
+d = 21.8.1947
+e = 7.10.1945
+print("Dad-", a)
+print("Mom-", b)
+print("Sister-", c)
+print("Grandma-", d)
+print("Grandpa-", e)
