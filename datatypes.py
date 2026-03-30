467bbe268db1a8bf6baf8468e65f774dd84db553
adyantwarang <waranganmol@gmail.com>
2026-03-30 20:33:47 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+#let's check out datatypes of different values
+
+a = 69
+print("type of a: ", type(a))
+b = 2.1
+print("type of b: ", type(b))
+c = "Python"
+print("type of c: ", type(c))
+d = True
+print("type of d: ", type(d))
