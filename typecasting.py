135ea6b9f48614d5fb0aa44bff903dfa2fe0c16d
adyantwarang <waranganmol@gmail.com>
2026-03-30 20:49:25 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,26 @@
+#Assigning different variables
+name = "Nairit"
+Age = 17
+is_student = True
+weight = 42.0
+
+#printing different variables and their data type
+print("name :", name)
+print("Data type of name is: ", type(name))
+print("Age :", Age)
+print("Data type of Age is: ", type(Age))
+print("is_student :", is_student)
+print("Data type of is_student is: ", type(is_student))
+print("weight :", weight)
+print("Data type of weight is: ", type(weight))
+
+#typecasting to converting the datatypes of variables
+print("\n After typecasting...")
+Age = str(Age)
+print(Age)
+print("Data type of age is: ", type(Age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is: ", type(weight))
+
+
