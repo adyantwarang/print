50156005820f50f421800e100f3441f0054cead5
adyantwarang <waranganmol@gmail.com>
2026-04-01 20:29:40 +0530

    Add files via upload

---
+++ operaters.py
@@ -0,0 +1,14 @@
+#Tree list
+tree1 = 69
+tree2 = 21
+tree3 = 67
+tree4 = 80
+tree5 = 42
+
+#finding the sum of the tree heights
+Sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all tree heights are: ",Sum)
+
+#finding the average
+Average = Sum/5
+print("The average of the tree heights are: ", Average)
