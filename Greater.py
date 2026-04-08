d06f59cbb52a98a98a6545466e6ecdac74e1f0e4
adyantwarang <waranganmol@gmail.com>
2026-04-08 20:58:25 +0530

    Add files via upload

---
+++ Greater.py
@@ -0,0 +1,10 @@
+num = float(input("Enter a number: "))
+
+#if and else statements
+if num > 15: 
+    print("This number is greater than 15")
+    print("I am in the if block")
+
+else: 
+    print("This number is less than 15")
+    print("I am in the else block")
