313db9859ca98a0fe8056e94519e909239460dc9
adyantwarang <waranganmol@gmail.com>
2026-04-13 20:36:48 +0530

    Add files via upload

---
+++ AndOrOperaters.py
@@ -0,0 +1,23 @@
+
+a = 10
+b = 20
+c = 0
+
+if a and b and c: 
+    print("All the numbers have the boolean value as true")
+else: 
+    print("At least one of the number have the boolean as false")
+
+A = 10
+B = -10
+C = 0
+
+if A > 0 or B > 0: 
+    print("Either of the number is greater than 0")
+else: 
+    print("No number is greater than 0")
+
+if B > 0 or C > 0: 
+    print("Either of the number is greater than 0")
+else: 
+    print("No number is greater than 0")
