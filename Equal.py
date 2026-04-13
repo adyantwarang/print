7e59265615ea94aaec36aacadf1e72c4fe85a968
adyantwarang <waranganmol@gmail.com>
2026-04-13 20:48:05 +0530

    Add files via upload

---
+++ Equal.py
@@ -0,0 +1,23 @@
+a = 25
+b = 21
+c = 19
+
+print(a != b)
+print(b != c)
+
+A = "python"
+B = "codingal"
+
+if A != B: 
+    print(A,'and', B, 'are different')
+
+i = 4
+ii = 5
+
+if (i == 1) != (ii == 5): 
+    print("Coolio")
+
+Num = int(input("Enter a number"))
+
+if Num%2 != 0: 
+    print(Num, "is not an even number")
