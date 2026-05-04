659a650f899ac174148d87ec32be0b6364539845
adyantwarang <waranganmol@gmail.com>
2026-05-04 21:11:10 +0530

    Add files via upload

---
+++ Spiral.py
@@ -0,0 +1,16 @@
+import turtle
+
+wm = turtle.Screen()
+wm.bgcolor("yellow")
+wm.title("Turtle")
+
+pen = turtle.Turtle()
+size = 0
+
+while True: 
+    for i in range(4): 
+        pen.fd(size + 1)
+        pen.left(90)
+
+        size = size - 5
+    size = size + 1
